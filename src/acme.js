d84a4411feaed3faa94420253e447ad3ef91994c
Masha Malygina <what4today@gmail.com>
2018-07-09 22:27:30 -0400

    second commit

--- src/acme.js
+++ src/acme.js
@@ -6,19 +6,22 @@ const acme = (sales, customers) => {
     sales.reduce((acc, e) => {
         if(Object.keys(temp).includes(e.customerID.toString())){
             temp[e.customerID] += e.total;
+
         } else {
             temp[e.customerID] = e.total;
         }
     }, 0);
     customers.reduce((acc, c) => {
-        if(Object.keys(temp).includes(c.id).toString()){
+        if(Object.keys(temp).includes((c.id).toString())){
             obj[c.name] = temp[c.id];
         }
         else {
-            throw 'the id in sales array does not have matching id in customer array';
+            obj[c.name] = 0;
         }
     }, 0);
+    console.log('resulting object ', obj);
     return obj;
 };
+
 
 module.exports = acme;
--- test/acme.spec.js
+++ test/acme.spec.js
@@ -6,9 +6,9 @@ describe('tests for acme function', () => {
     it('acme function exists', () => {
         expect(acme).to.be.ok;
     });
-    // it('acme returns object', () => {
-    //     expect(acme([], []).constructor.toString().slice(9, 14)).to.equal('Object');
-    // });
+    it('acme returns object', () => {
+        expect(acme([], []).constructor.toString().slice(9, 15)).to.equal('Object');
+    });
     it('accumulates correctly', () => {
         const salesData = [
             {
@@ -48,5 +48,62 @@ describe('tests for acme function', () => {
             }
         ];
         expect(acme(salesData, customerData)).to.eql({'moe': 8, 'betty': 5});
+    })
+    it('accumulates correctly with names missing from sales data, but present in customers data', () => {
+        const salesData = [
+            {
+                customerID: 1,
+                orderId: 1,
+                total: 2
+            },
+            {
+                customerID: 2,
+                orderId: 1,
+                total: 3
+            },
+            {
+                customerID: 1,
+                orderId: 2,
+                total: 5
+            },
+            {
+                customerID: 1,
+                orderId: 3,
+                total: 1
+            },
+            {
+                customerID: 3,
+                orderId: 1,
+                total: 6
+            },
+            {
+                customerID: 3,
+                orderId: 2,
+                total: 4
+            },            {
+                customerID: 2,
+                orderId: 2,
+                total: 2
+            }
+        ];
+        const customerData = [
+            {
+                id: 1,
+                name: 'moe'
+            },
+            {
+                id: 2,
+                name: 'betty'
+            },
+            {
+                id: 3,
+                name: 'joe'
+            },
+            {
+                id: 4,
+                name: 'andy'
+            }
+        ];
+        expect(acme(salesData, customerData)).to.eql({'moe': 8, 'betty': 5, 'joe': 10, 'andy': 0});
     })
 });
